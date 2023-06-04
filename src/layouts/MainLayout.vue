cac29875a95772fa6aedca0f24bea31381321201
João Pedro <jp.freitas202@gmail.com>
2023-06-03 21:25:20 -0300

    chore: base code

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title>
+          JL Papéis
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+})
+</script>
